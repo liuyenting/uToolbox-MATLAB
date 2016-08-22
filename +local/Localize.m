61f8d9ce42e6514eb2ec7815552c074a847147ed
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-08-22 18:33:44 +0800

    Complete the skeleton of Localize

--- +local/Localize.m
+++ +local/Localize.m
@@ -1,9 +1,25 @@
-classdef Localize < handle
-    
-    properties 
+classdef Localize < util.cppbridge.CppBridge
+    %LOCALIZE Perform sub-pixel localization.
+
+    properties (Constant, Access = private, Hidden = true)
+        MSG_ID = 'local:localize:matlab';
+        MEX_NAME = 'localizemex';
     end
-    
+
+    properties (Access = private)
+    end
+
     methods
+        function this = Localize()
+            this@util.cppbridge.CppBridge(mfilename('fullpath'), ...
+                                          local.Localize.MEX_NAME);
+        end
+    end
+
+    methods (Static, Access = protected)
+        function hdl = getfunchandle()
+            hdl = str2func(local.Localize.MEX_NAME);
+        end
     end
     
 end
