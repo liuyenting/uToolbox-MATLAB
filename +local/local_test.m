224581380ae4e02ba8f85039012cbf1c24b5138f
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-08-28 17:53:38 +0800

    Complete gaussian generation

--- +local/local_test.m
+++ +local/local_test.m
@@ -35,9 +35,26 @@ x = p(1:2:end);
 y = p(2:2:end);
       
 %% Fit the result.
+% Size of the bounding box.
+boxSize = 7;
+
+boxHalfSize = floor(boxSize/2);
 for i = 1:np
     x0 = x(i); 
     y0 = y(i);
+    
+    % Calculate the bounding box range, [Xmin, Xmax, Ymin, Ymax].
+    bBox = [x0 - boxHalfSize, x0 + boxHalfSize, ...
+            y0 - boxHalfSize, y0 + boxHalfSize];
+    % Extract the ROI.
+    roiImg = extractroi(fltImg, bBox);
+    % Skip this particle if valid ROI is too small.
+    if isempty(roiImg)
+        continue;
+    end
+    
+    % Fit the ROI by LSE.
+    [xf, yf] = 
 end
 
 end
@@ -99,4 +116,62 @@ I = double(I);
 J = round(J);
 J = uint16(J);
 
+end
+
+function J = extractroi(I, roi)
+%EXTRACTROI Extract region of interest from specified image.
+
+xMin = roi(1); 
+xMax = roi(2);
+yMin = roi(3); 
+yMax = roi(4);
+
+% Boundary check.
+[nx, ny] = size(I);
+if (xMin < 1) || (yMin < 1) || (xMax > nx) || (yMax > ny)
+    J = [];
+else
+    J = I(xMin:xMax, yMin:yMax);
+end
+
+end
+
+function parm = fitgauss(x, y)
+%FITGAUSS Fit the Gaussian peak.
+
+%% Default parameter.
+A_def = 1;
+x0_def = 
+
+end
+
+function F = gaussfunc(parm, data)
+%GAUSSFUNC Generate a 2-D Gaussian matrix using specified parameters.
+%
+%   Note: parm = [A, x0, wx, y0, wy, theta]
+%       A       Amplitude
+%       x0, y0  Center of the peak
+%       wx, wy  Spread of the peak
+%       theta   Rotation angle of the peak
+
+% Extract the parameter.
+A = parm(1);
+x0 = parm(2);
+y0 = parm(4);
+wx = parm(3);
+wy = parm(5);
+theta = parm(6);
+
+% Rotate the data points.
+dataRot(:, :, 1) = data(:, :, 1)*cos(theta) - data(:, :, 2)*sin(theta);
+dataRot(:, :, 2) = data(:, :, 1)*sin(theta) + data(:, :, 2)*cos(theta);
+
+% Rotate the estimated center.
+xRot = x0*cos(theta) - y0*sin(theta);
+yRot = x0*cos(theta) + y0*sin(theta);
+
+% Apply the Gaussian function.
+F = A * exp( -( ((dataRot(:, :, 1)-xRot).^2)/(2*wx^2) + ...
+                ((dataRot(:, :, 2)-yRot).^2)/(2*wy^2) ) );
+            
 end
