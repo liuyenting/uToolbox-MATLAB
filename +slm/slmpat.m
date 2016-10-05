13cdaf38e0940357c98852a9463eb580a8d7118b
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-10-05 20:43:28 +0800

    Modify the pattern generation routine

---
+++ +slm/slmpat.m
@@ -0,0 +1,18 @@
+function P = slmpat
+%SLMPAT Generates the SLM template.
+
+%% Parameters.
+% SLM dimension, [width, height] (px).
+slmDim = [2048, 2048];
+% Pixel size (um).
+pxSize = 8;
+% Oversampling factor.
+smplFactor = 1;
+% Wavelength (nm).
+wavelength = 561;
+% Number of repetitive patterns.
+nBeams = 41;
+
+
+end
+
