05a6fbcec73b31f88312e4e1a9f098eeccc1e00e
U-NanoSpeed\Alice <windows.linux.mac@gmail.com>
2017-04-30 15:38:30 +0800

    Binned the image based on desired output size

--- +resolution/binlocal.m
+++ +resolution/binlocal.m
@@ -1,18 +1,35 @@
-function I = binlocal(coords, npx, pxsz)
+function [I, pxsz] = binlocal(coords, sz)
 %BINLOCAL Bin the localization data
 %
 %   TBA
 
+% estimate proper pixel dimensions that can contain all the data
+pxsz = estpxsize(coords, sz);
+
 % generate the empty stack
-I = zeros(npx);
+I = zeros(sz);
 
 % convert coordinates to pixel scale
 pxsz = repmat(pxsz, [size(coords, 1), 1]);
 coords = coords ./ pxsz;
-coords = round(coords);
+coords = round(coords) + 1;
 
 % fill them to the stack
-ind = ndsub2ind(npx, coords);
-I(ind) = 1;
+ind = ndsub2ind(sz, coords);
+for i = ind
+    I(i) = I(i)+1;
+end
+
+end
+
+function pxsz = estpxsize(coords, sz)
+%ESTPXSIZE Estimate the pixel size of the binned image.
+%
+%   PXSZ = ESTPXSIZE(COORDS, SZ) estimates the generated pixel size 
+%   according to the maxmium size of the coordinate range.
+
+maxcoord = max(coords);
+pxsz = maxcoord ./ (sz-1);
+pxsz = repmat(max(pxsz(:)), size(sz));
 
 end
--- +resolution/private/ndsub2ind.m
+++ +resolution/private/ndsub2ind.m
@@ -5,6 +5,12 @@ function ind = ndsub2ind(sz, sub)
 k = cumprod(sz);
 k = [1, k(1:end-1)];
 
-ind = sub * k';
+% offset
+off = ones(size(k));
+off(1) = 0;
+off = repmat(off, [size(sub, 1), 1]);
+
+% conver to linear index to dot products
+ind = (sub-off) * k';
 
 end
