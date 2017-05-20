665500d0560a0bbfd3f371d96bc018ca8999f789
U-NanoSpeed\Alice <windows.linux.mac@gmail.com>
2017-05-20 19:18:18 +0800

    Create skeleton for spline DWT

---
+++ +filter/private/splinekern.m
@@ -0,0 +1,7 @@
+function [ output_args ] = splinekern( input_args )
+%SPLINEKERN Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ +filter/splinedwt.m
@@ -0,0 +1,7 @@
+function [ output_args ] = splinedwt( input_args )
+%SPLINEDWT Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
