ccdf964e86eb2ebef7211e927317b2914702bf50
U-NanoSpeed\Alice <windows.linux.mac@gmail.com>
2017-05-20 19:08:33 +0800

    Complete console logging feature

--- +util/consolelog.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = consolelog( input_args )
-%CONSOLELOG Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
---
+++ +util/timestamp.m
@@ -0,0 +1,35 @@
+function ts = timestamp(format)
+%TIMESTAMP Get timestamp.
+%   
+%   TS = TIMESTAMP create a string based on local time.
+%   TS = TIMESTAMP(FORMAT) can further control the format of the output.
+%   There are three types of plausible output:
+%       - 'DateTime':   'yyyy-MM-dd-HH-mm-ss'
+%       - 'Date':       'yyyy-MM-dd'
+%       - 'Time':       'HH-mm-ss'
+%
+%   Note
+%   ----
+%   Time zone is currently locked with the system. This can be expanded by
+%   passing additional parameter to the DATETIME function.
+%
+%   See also: DATETIME
+
+if nargin == 0
+    format = 'DateTime';
+end
+
+DATE_FORMAT = 'yyyy-MM-dd';
+TIME_FORMAT = 'HH-mm-ss';
+if strcmp(format, 'Time')
+    format = TIME_FORMAT;
+elseif strcmp(format, 'DateTime')
+    format = [DATE_FORMAT, '-', TIME_FORMAT];
+elseif strcmp(foramt, 'Date')
+    format = DATE_FORMAT;
+end
+
+ts = datetime('now', 'TimeZone', 'local', 'Format', format);
+
+end
+
