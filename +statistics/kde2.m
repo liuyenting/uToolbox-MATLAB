13ca196e32cfe0b74c450a8c50ae66d08bd8ae00
U-NanoSpeed\Alice <windows.linux.mac@gmail.com>
2017-05-30 11:07:05 +0800

    Add original source code comments

---
+++ +statistics/kde2.m
@@ -0,0 +1,22 @@
+function [bw, d, x, y] = kde2(data, n, min_xy, max_xy)
+%KDE2 Bivariate kernel density estimator with diagonal bandwidth matrix.
+%
+%   (From the original author) Fast and accurate state-of-the-art bivariate
+%   kernel density estimator with diagonal bandwidth matrix. The kernel is
+%   assumed to be Gaussian. The two bandwidth parameters are chosen
+%   optimally without ever assuming a parametric model for the data or any
+%   rule-of-thumb. Unlike many other procedures, this one is immune to
+%   accuracy failures in the estimation of multi-modal densities with
+%   widely separated modes.
+%
+%   TBA
+%   
+%   Reference
+%   ---------
+%   Kernel density estimation via diffusion
+%       Z. I. Botev, J. F. Grotowski, and D. P. Kroese (2010)
+%       Annals of Statistics, Volume 38, Number 5, pages 2916-2957.
+
+
+
+end 
