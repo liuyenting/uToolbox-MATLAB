1728bbe8e9cab5b6fc00a4bc99eed87a9c022707
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-07-04 23:39:49 +0800

    Set test data to platform dependent switch

--- sireconpp_demo.m
+++ sireconpp_demo.m
@@ -5,8 +5,18 @@
 close all;
 clearvars;
 
-srcDir = 'F:\\Andy\\06302017_SIM\\Tan1_3_3DSIM';
-psfFileName = 'F:\\Andy\\06302017_SIM\\PSF\\PSF_NA1p1_520nm_x100nm.tif';
+%TODO remove OS dependent test code
+if ispc
+    srcDir = 'F:\\Andy\\06302017_SIM\\Tan1_3_3DSIM';
+    psfFileName = 'F:\\Andy\\06302017_SIM\\PSF\\PSF_NA1p1_520nm_x100nm.tif';
+else
+    srcDir = 'data/sim/06302017_SIM/Tan1_3_3DSIM';
+    psfFileName = 'data/sim/06302017_SIM/PSF/PSF_NA1p1_520nm_x100nm.tif';
+
+    % prepend the root
+    srcDir = fullfile(userpath, srcDir);
+    psfFileName = fullfile(userpath, psfFileName);
+end
 
 dstSuffix = '_sirecon';
 dstDir = [srcDir, dstSuffix];
