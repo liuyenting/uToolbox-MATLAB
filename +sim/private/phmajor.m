033e02257cf01c786a675c5e55ed70dd1974862f
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-07-04 16:36:06 +0800

    Create skeleton for file iterations

---
+++ +sim/private/phmajor.m
@@ -0,0 +1,7 @@
+function B = phmajor(A, sz, no, np)
+%PHMAJOR Convert input N-D array to orientation-phase major.
+%   Detailed explanation goes here
+
+
+end
+
--- sireconpp_demo.m
+++ sireconpp_demo.m
@@ -3,13 +3,78 @@
 %   TBA
 
 close all;
-clearvar;
+clearvars;
 
 srcDir = 'F:\\Andy\\06302017_SIM\\Tan1_3_3DSIM';
 psfFileName = 'F:\\Andy\\06302017_SIM\\PSF\\PSF_NA1p1_520nm_x100nm.tif';
 
-%% load the file
+dstSuffix = '_sirecon';
+dstDir = [srcDir, dstSuffix];
 
+% orientations
+nOri = 1;
+% phases
+nPhase = 5;
 
+%% verify the input
+% check whether the input directory exsists
+if exist(srcDir, 'dir') ~= 7
+    error('sireconpp_demo', 'Input directory does not exist.');
+end
 
-%% reorder the shape
+% check whether the output directory is empty
+if exist(dstDir, 'dir') == 7
+    content = dir(dstDir);
+    if numel(content) > 2
+        error('sireconpp_demo', 'Output directory is not empty.');
+    end
+else
+    % create the directory
+    status = mkdir(dstDir);
+    if ~status
+        error('sireconpp_demo', 'Unable to create the output directory.');
+    end
+end
+
+%% load the file list
+%TODO only load 488nm for simplicity
+list = dir(fullfile(srcDir, '*488nm*.tif*'));
+list = {list.name};
+
+%% iterate through the files
+% ignore warnings for unknown tags for current session
+warning('off', 'MATLAB:imagesci:tiffmexutils:libtiffWarning');
+
+nFile = numel(list);
+tOuter = tic;
+for iFile = 1:nFile
+    tInner = tic;
+    
+    %% load the file
+    fileName = list{iFile};
+    fprintf('[%s]\n', fileName);
+    filePath = fullfile(srcDir, fileName);
+    
+    I = tiff.TIFFStack(filePath);
+    [nx, ny, nz] = size(I);
+    I = I(:);
+    I = reshape(I, nx, ny, nz);
+    
+    %% pre-process
+    % re-order the stack to phase-wise
+    
+    
+    if iFile == 1
+        %% find out kp value using the first stack
+    end
+    
+    %% reconstruction
+    
+    %% cleanup and statistics
+    fclose('all');
+    
+    tElapse = toc(tInner);
+    fprintf('%f seconds elapsed\n', tElapse);
+end
+tElapse = toc(tOuter);
+fprintf('%f seconds to process the incoming data\n', tElapse);
