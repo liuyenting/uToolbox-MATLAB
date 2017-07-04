3e47a89e52726ad3590e35debc6207b0768a1c98
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-07-04 17:20:24 +0800

    Fix O/P sequence during reshape

--- +sim/private/phmajor.m
+++
@@ -1,7 +0,0 @@
-function B = phmajor(A, sz, no, np)
-%PHMAJOR Convert input N-D array to orientation-phase major.
-%   Detailed explanation goes here
-
-
-end
-
--- sireconpp_demo.m
+++ sireconpp_demo.m
@@ -19,20 +19,21 @@ nPhase = 5;
 %% verify the input
 % check whether the input directory exsists
 if exist(srcDir, 'dir') ~= 7
-    error('sireconpp_demo', 'Input directory does not exist.');
+    error('sim:sireconpp_demo', 'Input directory does not exist.');
 end
 
 % check whether the output directory is empty
 if exist(dstDir, 'dir') == 7
     content = dir(dstDir);
     if numel(content) > 2
-        error('sireconpp_demo', 'Output directory is not empty.');
+        error('sim:sireconpp_demo', 'Output directory is not empty.');
     end
 else
     % create the directory
     status = mkdir(dstDir);
     if ~status
-        error('sireconpp_demo', 'Unable to create the output directory.');
+        error('sim:sireconpp_demo', ...
+              'Unable to create the output directory.');
     end
 end
 
@@ -47,6 +48,8 @@ warning('off', 'MATLAB:imagesci:tiffmexutils:libtiffWarning');
 
 nFile = numel(list);
 tOuter = tic;
+%OVERRIDE
+nFile = 1;
 for iFile = 1:nFile
     tInner = tic;
     
@@ -56,15 +59,41 @@ for iFile = 1:nFile
     filePath = fullfile(srcDir, fileName);
     
     I = tiff.TIFFStack(filePath);
-    [nx, ny, nz] = size(I);
-    I = I(:);
-    I = reshape(I, nx, ny, nz);
+    sz = size(I);
     
     %% pre-process
     % re-order the stack to phase-wise
-    
+    I = sim.phmajor(I, sz, nOri, nPhase);
     
     if iFile == 1
+        %% create calibration preview
+        calDir = fullfile(dstDir, 'calibrate');
+        
+        % create the directory
+        status = mkdir(calDir);
+        if ~status
+            error('sim:sireconpp_demo', ...
+                  'Unable to create the calibration output directory.');
+        end
+
+        % create MIP for debug
+        for iOri = 1:nOri
+            for iPhase = 1:nPhase
+                % extract the volume
+                P = I(iOri, iPhase, :, :, :);
+                P = squeeze(P);
+                
+                % MIP along Z axis
+                P = max(P, [], 1);
+                P = squeeze(P);
+                
+                % save the output
+                fname = sprintf('o%d_p%d.tif', iOri, iPhase);
+                fpath = fullfile(calDir, fname);
+                tiff.imsave(P, fpath);
+            end
+        end
+        
         %% find out kp value using the first stack
     end
     
