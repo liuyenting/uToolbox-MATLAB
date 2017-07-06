f58100c60fbd3126ad3766900725353e962e2fa8
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-07-06 16:44:51 +0800

    Include apodization function

--- +sim/private/sireconppcore.m
+++ +sim/private/sireconppcore.m
@@ -2,14 +2,55 @@ function J = sireconppcore(I, imSz, M, kp, parms)
 %SIRECONPPCORE Summary of this function goes here
 %   Detailed explanation goes here
 
+persistent A;
+
 % extract frequent use parameters
 nOri = parms.Orientations;
 nPhase = parms.Phases;
 psz = siparms.PadSize;
 
+% create the apodization function if not exists
+if isempty(A)
+    %TODO cosine mask
+    
+    % reuse for each phase
+    A = repmat(A, [nPhase, imSz]);
+end
+
+% buffer space for results from the frequency domain
+F = zeros([nPhase, imSz], 'single');
 for iOri = 1:nOri
     for iPhase = 1:nPhase
+        % extract volume
+        T = I(iOri, iPhase, :, :);
+        T = squeeze(T);
+        
+        % pad the surrounding sides
+        T = padarray(T, [psz, psz], 0, 'both');
+        % RL deconvolution
+        T = deconvlucy(T, parms.PSF, parms.PreDeconv);
+        % crop the result
+        T = T(psz+1:end-psz, psz+1:end-psz);
+        
+        % FT
+        T = fftshift(fft2(T));
+        
+        % save the result
+        F(iPhase, :, :) = T;
     end
+    
+    %% retrieve domains
+    % flatten the array
+    F = reshape(F, [iPhase, prod(imSz)]);
+    % solve the matrix
+    F = M \ F;
+    % reshape back to original image size
+    F = reshape(F, [iPhase, imSz]);
+    
+    %% find phases
+    % multiply apodization function
+    F = F .* A;
+    
 end
 
 end
