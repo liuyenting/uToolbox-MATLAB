182e84d7c53d9619ec2424b606c2837457c13f49
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-07-10 18:28:00 +0800

    Isolate m0 term during the phase retrieval

--- +sim/private/sireconppcore.m
+++ +sim/private/sireconppcore.m
@@ -68,7 +68,8 @@ for iOri = 1:nOri
     R(:, offset(1):offset(1)+imSz(1)-1, offset(2):offset(2)+imSz(2)-1) = F;
     
     % reference frame
-    R(1, :, :) = fftshift(ifft2(ifftshift(squeeze(R(1, :, :)))));
+    T = fftshift(ifft2(ifftshift(squeeze(R(1, :, :)))));
+    R(1, :, :) = abs(T);
     
     % revert from position to shift
     shift = (squeeze(kp(iOri, :, :)) - repmat(imSz/2, [nPhase-1, 1]));
@@ -82,7 +83,7 @@ for iOri = 1:nOri
     [vx, vy] = meshgrid(1:rSz(1), 1:rSz(2));
     vx = vx - (rSz(1)+1);
     vy = vy - (rSz(2)+1);
-    pr = zeros([nPhase-1, rSz]);
+    pr = zeros([nPhase-1, rSz], 'single');
     for iPhase = 1:nPhase-1
         % distance matrix
         D = vx*shift(iPhase, 1) + vy*shift(iPhase, 2);
@@ -92,29 +93,40 @@ for iOri = 1:nOri
     
     % apply nonlinear optimization
     problem = struct;
-    problem.objective = @(x) costfunc(R, rSz, x, pr);
-    problem.x0 = zeros([1, (nPhase-1)/2]);
+    problem.objective = @(x) costfunc(R(1, :, :), R(2:end, :, :), rSz, x, pr);
+    problem.x0 = [0.01, 0.01];
     problem.Aineq = [];
     problem.bineq = [];
     problem.Aeq = [];
     problem.beq = [];
-    problem.lb = zeros([1, (nPhase-1)/2]);
-    problem.ub = repmat(2*pi, [1, (nPhase-1)/2]);
+    problem.lb = zeros(size(problem.x0));
+    problem.ub = (2*pi) * ones(size(problem.x0));
     problem.nonlcon = [];
     problem.solver = 'fmincon';
-    problem.options = [];
+    % additional options
+    options = optimoptions( ...
+        'fmincon', ...
+        'Display', 'iter-detailed' ...
+    );
+    problem.options = options;
     p = fmincon(problem);
 end
 
 end
 
-function C = costfunc(Rp, sz, p0, pr)
+function C = costfunc(R0, Rp, sz, p0, pr)
 %COSTFUNC Cost function to minimize for the phase retrieval algorithm.
 %
 %   sz: image size
 %   Rp: frequency plane
 %   p0: initial phase shift
 %    p: relative phsae shift, determined by kp
+
+persistent h;
+
+if isempty(h) || ~isvalid(h)
+    h = figure('Name', 'Phase Retrieval', 'NumberTitle', 'off');
+end
 
 np = length(p0);
 
@@ -127,20 +139,32 @@ p0 = repmat(p0, [prod(sz), 1]);
 p0 = reshape(p0, [sz, np]);
 p0 = permute(p0, [3, 1, 2]);
 % shift the frequency plains to their correct locations
-Rp(2:end, :, :) = Rp(2:end, :, :) .* p0;
+Rp = Rp .* p0;
 
 % revert back to real space
-for ip = 2:np
+for ip = 1:np
     Rp(ip, :, :) = fftshift(ifft2(ifftshift(squeeze(Rp(ip, :, :))))); 
 end
 
 % apply modulation pattern
-Rp(2:end, :, :) = Rp(2:end, :, :) .* pr;
+Rp = Rp .* pr;
 
 % sum the result to evaluate performance
-C = abs(sum(Rp, 1));
+C = abs(sum([R0; Rp], 1));
+% squeeze the dimension
+C = squeeze(C);
+
+figure(h);
+imagesc(C);
+    axis image;
+drawnow;
+pause(0.5);
+
 % maximize the function, use negative sign to use fmin* optimizer
-C = squeeze(Rp(1, :, :)) .* C;
+C = R0 .* C;
 C = -sum(C(:));
+
+% output is required to be double instead of single
+C = double(C);
 
 end
