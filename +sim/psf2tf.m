aedf6348454fd379de01df823574c3dc41af55ff
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-07-23 09:57:55 +0800

    Apply modulation pattern to OTF

--- +sim/psf2tf.m
+++ +sim/psf2tf.m
@@ -19,6 +19,8 @@ else
           'Unsupported type of SIM data source.');
 end
 
+psfSz = size(PSF);
+
 pxSz = parms.PixelSize;
 
 n = parms.RefractiveIndex;
@@ -27,21 +29,17 @@ lambda = parms.Wavelength;
 %% pre-calculate
 OTF = psf2otf(PSF, imSz);
 
+hPre = figure('Name', 'Illumination Pattern', 'NumberTitle', 'off');
+hPost = figure('Name', 'Transfer Function', 'NumberTitle', 'off');
+
 %% pre-allocate
 TF = zeros([imSz, nPhase, nOri], 'single');
 
 % first phases require no special treatments
 TF(:, :, 1, :) = repmat(OTF, [1, 1, 1, nOri]);
 
-% modulated illumination plane
-Im = zeros(imSz, 'single');
-
 % the grid for computation
-[vx, vy] = meshgrid(1:imSz(1), 1:imSz(2));
-% offset to center
-midpt = floor(imSz/2)+1;
-vx = vx-midpt(1);
-vy = vy-midpt(2);
+[vx, vy] = meshgrid(1:psfSz(1), 1:psfSz(2));
 % convert to real word scale
 vx = vx * pxSz(1);
 vy = vy * pxSz(2);
@@ -70,10 +68,13 @@ for iOri = 1:nOri
             %   sin(theta) = lambda / (2*n*p)
             %              = lambda / (2*n) * f
             %
+            % for m1 terms, interference frequency is doubled, hence
+            %   sin(theta) = lambda / n * f
+            %
             % kz = (1-cos(theta)) * n/lambda
             %    = (1-sqrt(1-sin(theta)^2)) * n/lambda
-            %    = n/lambda - sqrt((n/lambda)^2 - (f/2)^2)
-            y = n/lambda - sqrt((n/lambda)^2 - (f/2)^2);
+            %    = n/lambda - sqrt((n/lambda)^2 - f^2)
+            y = n/lambda - sqrt((n/lambda)^2 - f^2);
         else
             y = 0;
         end
@@ -92,21 +93,47 @@ for iOri = 1:nOri
             k = [x; y];
         end
         
-        % Since we have two possible k formation, shift them to ensure we
-        % will not collide with the image dimension (X and Y).
-        k = reshape(k, 2, 1, []);
+        % turns into the actual wave number
+        %   k = 2*pi / p
+        %     = 2*pi * f
+        % Note: While p is period and f is frequency, we are working 
+        % backward from the reciprocal space, hence f is used instead of p.
+        k = 2*pi * k;
         
-        % turns into cycle (wave number)
-        k = (2*pi) / k;
-        
-        % Note: Implicit dimension expansion is used here.
-        %Im = 1 + cos(vx.*k(1, :, :)) .* cos(vy.*k(2, :, :));
-        Im = 1 + cos(vx.*k(1, :, 1));
-        
-        % merge the result if we are working with splitted bands
+        % splitted bands require additional merging
+        % Note: Implicit dimension expansion is used in the wave function.
         if isSplitted
+            k = reshape(k, 2, 1, []);
+            Im = 1 + real(exp(1i * (vx.*k(2, :, :) + vy.*k(1, :, :))));
             Im = sum(Im, 3);
+        else
+            Im = 1 + real(exp(1i * (vx.*k(2) + vy.*k(1))));
         end
+        
+        %DEBUG
+        figure(hPre);
+        subplot(1, 2, 1);
+        imagesc(Im);
+            axis image;
+            title('R-Space');
+        subplot(1, 2, 2);
+        imagesc(abs(fftshift(fft2(ifftshift(Im)))).^0.7);
+            axis image;
+            title('K-Space');
+            
+        %% apply to system OTF
+        TF(:, :, iPhase, iOri) = psf2otf(PSF .* Im, imSz);
+        
+        %DEBUG
+        figure(hPost);
+        subplot(1, 2, 1);
+        imagesc(PSF);
+            axis image;
+            title('System PSF');
+        subplot(1, 2, 2);
+        imagesc(abs(ifftshift(TF(:, :, iPhase, iOri))).^0.7);
+            axis image;
+            title('Modulated');
     end
 end
 
